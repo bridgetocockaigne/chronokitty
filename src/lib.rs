a1c80d6c49f6760e45179d086ee2542d50a763fb
iamricardobatista <129659332+iamricardobatista@users.noreply.github.com>
2023-06-11 10:42:20 +0100

    feat: start and stop tasks (#6)

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,43 @@ use chrono::{DateTime, Duration, Utc};
 #[derive(Debug, Default)]
 pub struct Task {
     pub name: String,
-    pub start_time: DateTime<Utc>,
+    start_time: Option<DateTime<Utc>>,
+    stop_time: Option<DateTime<Utc>>,
 }
 
 impl Task {
     pub fn new(name: String) -> Self {
         Self {
             name,
-            start_time: Utc::now(),
+            start_time: None,
+            stop_time: None,
         }
     }
 
-    pub fn elapsed_time(&self) -> Duration {
-        Utc::now() - self.start_time
+    pub fn start(&mut self) {
+        self.start_time = Some(Utc::now());
+    }
+
+    pub fn stop(&mut self) {
+        self.stop_time = Some(Utc::now());
+    }
+
+    pub fn is_started(&self) -> bool {
+        self.start_time.is_some()
+    }
+
+    pub fn is_stoped(&self) -> bool {
+        self.stop_time.is_some()
+    }
+
+    pub fn duration(&self) -> Duration {
+        match self {
+            Task {
+                start_time: Some(start_time),
+                stop_time: Some(stop_time),
+                ..
+            } => *stop_time - *start_time,
+            _ => Duration::zero(),
+        }
     }
 }
--- tests/chronokitty.rs
+++ tests/chronokitty.rs
@@ -1,28 +1,44 @@
 use std::usize;
 
 use chrono::Duration;
-use cucumber::{then, when, World};
+use cucumber::{given, then, when, World};
 
 #[derive(Debug, Default, World)]
 pub struct ChronoKittyWorld {
     task: chronokitty::Task,
 }
 
-#[when(expr = "the tracker starts a new task with name {string}")]
-async fn tracker_stars_a_new_task(w: &mut ChronoKittyWorld, name: String) {
+#[given(expr = "a task with name {string}")]
+async fn given_a_task(w: &mut ChronoKittyWorld, name: String) {
     w.task = chronokitty::Task::new(name);
 }
 
-#[then(expr = "the tracked time is greater then {int}")]
-async fn the_tracked_time_is_greater_then(w: &mut ChronoKittyWorld, time: i64) {
-    let duration = Duration::seconds(time);
+#[when(expr = "the tracker starts the task")]
+async fn the_tracker_starts_the_task(w: &mut ChronoKittyWorld) {
+    w.task.start();
+}
+#[then(expr = "the task is started")]
+async fn the_task_is_started(w: &mut ChronoKittyWorld) {
+    assert!(w.task.is_started());
+}
+#[when(expr = "the tracker stops the task")]
+async fn the_tracker_stops_the_task(w: &mut ChronoKittyWorld) {
+    w.task.stop();
+}
 
-    assert!(
-        w.task.elapsed_time().gt(&duration),
-        "the tracked time was not greater than {}, elipsed time: {}",
-        time,
-        w.task.elapsed_time()
-    )
+#[then(expr = "the task is stoped")]
+async fn the_task_is_stoped(w: &mut ChronoKittyWorld) {
+    assert!(w.task.is_stoped());
+}
+
+#[then(expr = "the task have a duration bigger then zero")]
+async fn the_task_have_duration_bigger_then(w: &mut ChronoKittyWorld) {
+    assert!(w.task.duration().gt(&Duration::zero()))
+}
+
+#[then(expr = "the task have a duration is zero")]
+async fn the_task_have_duration_is_zero(w: &mut ChronoKittyWorld) {
+    assert!(w.task.duration().eq(&Duration::zero()))
 }
 
 #[tokio::main]
