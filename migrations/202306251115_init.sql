06d87913f315032ddfc90dac0fb6a43c03c04eaf
iamricardobatista <129659332+iamricardobatista@users.noreply.github.com>
2023-06-25 12:37:49 +0100

    feat: storage (#11)
    * snapshot: storage features
    * snap: more code juggle
    * feat: first migration

---
+++ migrations/202306251115_init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS tasks
+(
+    id      INTEGER PRIMARY KEY NOT NULL,
+    name,
+    start_date,
+    end_date,
+    labels
+);
--- tests/chronokitty.rs
+++ tests/chronokitty.rs
@@ -1,6 +1,8 @@
+mod storage;
 mod task;
 
 #[tokio::main]
 async fn main() {
     task::run().await;
+    storage::run().await;
 }
